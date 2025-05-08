0268e1b680d0fbd4f3d9fc72759454c60271fcc2
Thais Costa <thais1fc2002@gmail.com>
2025-05-07 22:45:59 -0300

    primeiro commit login thais

---
+++ Login/script.js
@@ -0,0 +1,30 @@
+
+document.getElementById("formLogin").addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+
+
+  try { //COLOCAR A API AQUI
+    const resposta = await fetch("https://68195b3c1ac1155635049727.mockapi.io/api/projetofinal/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Bearer: token.token
+      },
+      body: JSON.stringify({ email, senha }),
+    });
+
+    const dados = await resposta.json();
+
+    if (resposta.ok) {
+      alert("Login realizado com sucesso!");
+      window.location.href = "../Home/index.html";
+    } else {
+      alert("Erro ao realizar login!!! ");
+    }
+  } catch (error) {
+    alert("Erro ao conectar com o servidor !!!");
+  }
+});
